05a32ad2af2a2100937907225cf1eae1149a490c
mynolog <minholee.log@gmail.com>
2025-07-28 09:24:16 +0900

    fix: 환경 별 Google  서비스 계정 JSON 경로 분기 처리 수정 (개발/ 배포 환경 대응)

--- src/services/googleSheets.ts
+++ src/services/googleSheets.ts
@@ -2,8 +2,13 @@ import { google } from "googleapis";
 import path from "path";
 import { ENV } from "../config/env";
 
+const serviceAccountPath =
+  process.env.NODE_ENV === "development"
+    ? path.join(__dirname, "../../service-account.json")
+    : "/etc/secrets/service-account.json";
+
 const auth = new google.auth.GoogleAuth({
-  keyFile: path.join(__dirname, "../../service-account.json"),
+  keyFile: serviceAccountPath,
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
