fe3a427c07c750fc3068929388a46b23d988d392
mynolog <minholee.log@gmail.com>
2025-07-31 09:02:33 +0900

    fix: 리마인더 스케줄러 KST 시간대 반영되도록 알림 시간대 수정

--- src/jobs/reminderScheduler.ts
+++ src/jobs/reminderScheduler.ts
@@ -11,13 +11,13 @@ interface ReminderConfig {
 
 const reminders: ReminderConfig[] = [
   {
-    cronTime: "0 11 * * 6",
+    cronTime: "0 20 * * 6",
     channelId: ENV.POSTING_CHANNEL_ID,
     message: () =>
       `@everyone 이번 주 글쓰기 제출 기한은 ${getThisSundayDateString()} 23:59까지 입니다. 아직 제출하지 않으셨다면 포스팅 제출 잊지 마세요!`,
   },
   {
-    cronTime: "0 11 * * 4",
+    cronTime: "0 20 * * 3",
     channelId: ENV.VACATION_CHANNEL_ID,
     message: () =>
       `@everyone 이번 주 휴가 신청은 마감되었습니다. 이후 신청은 반영되지 않습니다.`,
@@ -26,18 +26,22 @@ const reminders: ReminderConfig[] = [
 
 export function startRemiderScheduler(client: Client) {
   for (const { cronTime, channelId, message } of reminders) {
-    cron.schedule(cronTime, async () => {
-      try {
-        const channel = await client.channels.fetch(channelId);
-        if (channel?.isTextBased()) {
-          (channel as TextChannel).send({
-            content: message(),
-            allowedMentions: { parse: ["everyone"] },
-          });
+    cron.schedule(
+      cronTime,
+      async () => {
+        try {
+          const channel = await client.channels.fetch(channelId);
+          if (channel?.isTextBased()) {
+            (channel as TextChannel).send({
+              content: message(),
+              allowedMentions: { parse: ["everyone"] },
+            });
+          }
+        } catch (error) {
+          console.error("리마인드 메시지 전송 실패:", error);
         }
-      } catch (error) {
-        console.error("리마인드 메시지 전송 실패:", error);
-      }
-    });
+      },
+      { timezone: "Asia/Seoul" }
+    );
   }
 }
